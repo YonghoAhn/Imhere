8b1d5808985fd4ab1080cc8ba9b4d5bf783cf44d
chicken2039 <zoq2039@gmail.com>
2020-07-08 03:55:59 +0900

    # add AgeRangeCheck # edit SetAgeFragment.kt # edit strings.xml

--- app/src/main/java/moe/misakachan/imhere/SetAgeFragment.kt
+++ app/src/main/java/moe/misakachan/imhere/SetAgeFragment.kt
@@ -2,13 +2,20 @@ package moe.misakachan.imhere
 
 import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.Toast
+import androidx.annotation.IntegerRes
+import androidx.core.widget.addTextChangedListener
 import androidx.navigation.fragment.findNavController
 import androidx.transition.TransitionInflater
 import kotlinx.android.synthetic.main.fragment_set_age.*
+import java.lang.Integer.parseInt
 
 
 class SetAgeFragment : Fragment() {
@@ -17,6 +24,9 @@ class SetAgeFragment : Fragment() {
         super.onCreate(savedInstanceState)
         sharedElementEnterTransition = TransitionInflater.from(context)
             .inflateTransition(android.R.transition.move)
+
+
+
     }
 
     override fun onCreateView(
@@ -30,11 +40,20 @@ class SetAgeFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         button.setOnClickListener {
-            requireContext().getSharedPreferences("imhere", Context.MODE_PRIVATE).edit().putString("age",
-                txtAge.text.toString()
-            ).apply()
-            findNavController().navigate(R.id.action_setAgeFragment_to_mainFragment)
+            var age = parseInt(txtAge.text.toString());
+            if (age in 4..120) {
+                requireContext().getSharedPreferences("imhere", Context.MODE_PRIVATE).edit()
+                    .putString(
+                        "age",
+                        txtAge.text.toString()
+                    ).apply()
+                findNavController().navigate(R.id.action_setAgeFragment_to_mainFragment)
+            }
+            else{
+                Toast.makeText(requireContext(),R.string.error_age, Toast.LENGTH_SHORT).show()
+            }
         }
     }
+
 
 }
